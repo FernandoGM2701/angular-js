6623545e7a907df13ca698b813f2a90632baa97a
FernandoGM2701 <fernandogm2701@gmail.com>
2022-09-22 23:28:55 -0500

    [Add] ng repeat 2

--- Ejercicio3/js/app.js
+++ Ejercicio3/js/app.js
@@ -5,7 +5,22 @@
     
         $scope.listNames = [ "Fernando Gutierrez", "Enrique Bunbury", "Joaquin Ramirez" ];
 
-
+        $scope.listTeams = {
+            team: [{
+                name: "Deportivo Municipal",
+                alias: "Echa Muni",
+                fundation: 1935
+            }, {
+                name: "Real Madrid",
+                alias: "Madrid",
+                fundation: 1960
+            }, {
+                name: "Barcelona",
+                alias: "Barza",
+                fundation: 1980
+            }
+            ]
+        }
     } ]);
 
 }) ();
