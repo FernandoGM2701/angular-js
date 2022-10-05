14c4f2c06b704ef08f580e044f6ace72b7ef51f9
FernandoGM2701 <fernandogm2701@gmail.com>
2022-10-04 23:25:27 -0500

    [Add] ng-options

--- ng-options/js/app.js
+++ ng-options/js/app.js
@@ -1,29 +1,30 @@
-(function() {
-  
-    var app = angular.module("appFilterNgOptions", [ ]);
+(function () {
+  var app = angular.module("appFilterNgOptions", []);
 
-    app.controller('ngOptionsController', ['$scope', function($scope) {
-    
-        $scope.countries = [
-          { id:"CRI", nombre:"COSTA RICA"},
-          { id:"HRV", nombre:"CROACIA"},
-          { id:"CUB", nombre:"CUBA"},
-          { id:"DNK", nombre:"DINAMARCA"},
-          { id:"DMA", nombre:"DOMINICA"},
-          { id:"DOM", nombre:"REPÚBLICA DOMINICANA"},
-          { id:"ECU", nombre:"ECUADOR"},
-          { id:"EGY", nombre:"EGIPTO"},
-          { id:"SLV", nombre:"EL SALVADOR"},
-          { id:"ARE", nombre:"EMIRATOS ÁRABES UNIDOS"},
-          { id:"ERI", nombre:"ERITREA"},
-          { id:"SVK", nombre:"ESLOVAQUIA"},
-          { id:"SVN", nombre:"ESLOVENIA"},
-          { id:"ESP", nombre:"ESPAÑA"},
-          { id:"USA", nombre:"ESTADOS UNIDOS"},
-          { id:"EST", nombre:"ESTONIA"}
-        ];
+  app.controller("ngOptionsController", ['$scope', function($scope){
 
-        $scope.countrieSelected = "ECU";
-    } ]);
+      $scope.countrySelected = "ECU";
 
-}) ();
+      $scope.countries = [
+        { id: "CRI", nombre: "COSTA RICA" },
+        { id: "HRV", nombre: "CROACIA" },
+        { id: "CUB", nombre: "CUBA" },
+        { id: "DNK", nombre: "DINAMARCA" },
+        { id: "DMA", nombre: "DOMINICA" },
+        { id: "DOM", nombre: "REPÚBLICA DOMINICANA" },
+        { id: "ECU", nombre: "ECUADOR" },
+        { id: "EGY", nombre: "EGIPTO" },
+        { id: "SLV", nombre: "EL SALVADOR" },
+        { id: "ARE", nombre: "EMIRATOS ÁRABES UNIDOS" },
+        { id: "ERI", nombre: "ERITREA" },
+        { id: "SVK", nombre: "ESLOVAQUIA" },
+        { id: "SVN", nombre: "ESLOVENIA" },
+        { id: "ESP", nombre: "ESPAÑA" },
+        { id: "USA", nombre: "ESTADOS UNIDOS" },
+        { id: "EST", nombre: "ESTONIA" },
+      ];
+
+
+    },
+  ]);
+})();
